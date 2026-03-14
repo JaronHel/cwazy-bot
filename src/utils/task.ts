e4dfabfea37c07948720e68310cc5a0a562c5030
JaronHel <jaronhellenbrand07@gmail.com>
2026-03-14 17:08:09 +0100

    sending buffer instead of file

--- src/utils/task.ts
+++ src/utils/task.ts
@@ -1,5 +1,4 @@
 import { Client, TextChannel } from "discord.js";
-import fs from "fs/promises";
 import { Balance } from "../models/balance.model";
 
 export function startBackupTask(client: Client) {
@@ -13,21 +12,20 @@ async function databaseBackup(client: Client) {
 
     const balances = await Balance.find();
 
-    const filePath = "./balances.json";
+    const buffer = Buffer.from(JSON.stringify(balances));
 
-    await fs.writeFile(filePath, JSON.stringify(balances));
-
-    await sendBackup(client, filePath);
+    const channel = await client.channels.fetch("1482349060407951442");
+    if (!(channel instanceof TextChannel)) return;
+    await channel.send({
+      content: "Balance backup",
+      files: [
+        {
+          attachment: buffer,
+          name: "balances.json",
+        },
+      ],
+    });
   } catch (err: any) {
     console.error("Backup failed: " + err);
   }
-}
-
-async function sendBackup(client: Client, filePath: string) {
-  const channel = await client.channels.fetch("1482349060407951442");
-  if (!(channel instanceof TextChannel)) return;
-  await channel.send({
-    content: "Balance backup",
-    files: [filePath],
-  });
 }
